b1f40746a9a7892e37e1f399cd06ce8321db6565
Honey Singh <Honeysingh.397573@gmail.com>
2026-06-24 17:50:34 +0530

    this is my first file of OOPs learning

---
+++ creating_class_and_object.py
@@ -0,0 +1,23 @@
+# creating a class
+
+class Student:
+    name = "honey"
+# Creating an object, objects are bound with the classes.
+
+S1 = Student
+print(S1.name)
+
+# Class is a Blueprint which has all the attributes(Data).
+# It also defines the functionality or methods for objects.
+
+# Object => Object is a real world entity represented in code.
+
+
+Fruits = ["Mango" , "Banana", "Apple"]
+print(type(Fruits))
+print(Fruits)
+
+# Fruits is an object of a class.
+
+
+
